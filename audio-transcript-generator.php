d2018db4026213dcf2584b2be764258ff9947203
stronganchor <77190959+stronganchor@users.noreply.github.com>
2025-03-24 09:27:35 +0300

    Update audio-transcript-generator.php

--- audio-transcript-generator.php
+++ audio-transcript-generator.php
@@ -3,7 +3,7 @@
 Plugin Name: AI Audio Transcription Interface
 Plugin URI: https://stronganchortech.com
 Description: A plugin to handle audio transcription using the AssemblyAI API via a URL input field.
-Version: 1.9.8
+Version: 1.9.9
 Author: Strong Anchor Tech
 Author URI: https://stronganchortech.com
 */
